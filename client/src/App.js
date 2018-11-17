36d6566565cb02c17f08cf345e984a4a1cc9467c
Young Do Kim <yzk5147@gmail.com>
2018-11-16 20:57:36 -0500

    redirect / to /home

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Switch, Route, NavLink } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import About from './components/About';
 import Contact from './components/Contact';
 import Home from './components/Home';
@@ -8,19 +8,19 @@ import Home from './components/Home';
 const Navigation = () => (
   <nav class="navbar">
     <div class="container">    
-      <h1 class="logo"><a href="home">Simple Navbar</a></h1>
+      <h1 class="logo"><a href="/home">Simple Navbar</a></h1>
       <ul class="nav nav-right">
-        <li><a href="home">Home</a></li>
-        <li><a href="about">About</a></li>
-        <li><a href="contact">Contact</a></li>
+        <li><a href="/home">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/  contact">Contact</a></li>
       </ul>
     </div>
-    
   </nav>
 );
 
 const Main = () => (
   <Switch>
+    <Route exact path='/' component={Home}></Route>
     <Route exact path='/home' component={Home}></Route>
     <Route exact path='/about' component={About}></Route>
     <Route exact path='/contact' component={Contact}></Route>
